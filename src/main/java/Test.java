97b56a65f5b2b4ee14c40a863650448a2328b238
william meza <wmezasoria@yahoo.com>
2023-05-06 13:12:36 -0500

    initial commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,8 @@ public class Test {
     public static void main(String[] args) {
         //printing hello world
         System.out.println("Hello World");
+        //printing local date time
+        System.out.println(java.time.LocalDateTime.now());
+
     }
 }
